4681169702aafee0f1adc472311e3111c273313b
jiajunh <jiajunh@umich.edu>
2025-02-05 15:42:49 -0500

    batch 8->16

--- Qwen2.5-Math-main/evaluation/math_eval.py
+++ Qwen2.5-Math-main/evaluation/math_eval.py
@@ -284,7 +284,7 @@ def main(llm, tokenizer, data_name, args):
                 tokenizer=tokenizer,
                 prompts=prompts,
                 max_new_tokens=args.max_tokens_per_call,
-                batch_size=8,
+                batch_size=16,
                 stop_id_sequences=stop_words,
             )
 
