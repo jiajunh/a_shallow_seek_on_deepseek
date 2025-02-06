bdc1532a915fa62964ff971703dc83a87f940130
jiajunh <jiajunh@umich.edu>
2025-02-05 19:30:48 -0500

    fixing the vllm version

--- Qwen2.5-Math-main/evaluation/sh/install.sh
+++ Qwen2.5-Math-main/evaluation/sh/install.sh
@@ -3,9 +3,10 @@
 cd ../latex2sympy
 pip install -U pip setuptools
 pip install torch torchvision torchaudio
+pip3 install xformers==0.0.27.post2 --index-url https://download.pytorch.org/whl/cu121
 
 pip install -e .
 cd ..
 pip install -r requirements.txt 
-pip install vllm==0.4.2 --no-build-isolation
+pip install vllm==0.5.4 --no-build-isolation
 pip install transformers==4.42.3
--- Qwen2.5-Math-main/evaluation/sh/install_test.sh
+++ Qwen2.5-Math-main/evaluation/sh/install_test.sh
@@ -2,11 +2,12 @@
 
 cd ../latex2sympy
 pip install -U pip setuptools
-# pip install torch torchvision torchaudio
+
 pip install torch torchvision torchaudio
+pip3 install xformers==0.0.27.post2 --index-url https://download.pytorch.org/whl/cu121
 
 pip install -e .
 cd ..
 pip install -r requirements_test.txt 
-pip install vllm==0.4.2 --no-build-isolation
+pip install vllm==0.5.4 --no-build-isolation
 pip install transformers==4.42.3
