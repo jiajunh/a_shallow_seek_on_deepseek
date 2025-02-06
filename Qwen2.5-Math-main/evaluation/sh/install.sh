b94503e7b4004de1821aa013ade93e5bd717931b
jiajunh <jiajunh@umich.edu>
2025-02-05 19:07:22 -0500

    special pytorch version for cuda 12.1

--- Qwen2.5-Math-main/evaluation/sh/install.sh
+++ Qwen2.5-Math-main/evaluation/sh/install.sh
@@ -2,10 +2,10 @@
 
 cd ../latex2sympy
 pip install -U pip setuptools
-pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu121
+pip install torch torchvision torchaudio
 
 pip install -e .
 cd ..
 pip install -r requirements.txt 
-pip install vllm==0.5.1 --no-build-isolation
+pip install vllm==0.4.2 --no-build-isolation
 pip install transformers==4.42.3
